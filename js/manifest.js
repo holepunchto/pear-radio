ac396b6180d4b77169358cb32bac81a7cef48f02
rafapaezbas <rafa@holepunch.com>
2023-12-28 17:34:47 +0100

    backend refactor

--- js/manifest.js
+++
@@ -1,9 +0,0 @@
-export function createManifest (publicKey, namespace) {
-  return {
-    signer: {
-      signature: 'ed25519',
-      publicKey,
-      namespace: Buffer.from(namespace.padEnd(32, '\0'))
-    }
-  }
-}
